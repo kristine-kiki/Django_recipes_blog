3e98e2414f20e98d0602518c7e314e3c51ebf307
Kristine Kamola <kristine.kamola@gmail.com>
2025-01-21 19:03:17 +0000

    add PostgreSQL and create superuser

--- headchef/settings.py
+++ headchef/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-y@w=ss2uw8)t4@d*u8)-f##&+d046en4y)*!p+-h&$ukkk5m2t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-kristinekik-djangorecip-7rss1atlxp9.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-kristinekik-djangorecip-7rss1atlxp9.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
@@ -74,11 +78,15 @@ WSGI_APPLICATION = 'headchef.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 

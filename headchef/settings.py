cfc688dee5608ab91b7c9becdbefc616158b4df1
Kristine <kristine.kamola@gmail.com>
2025-02-04 18:33:18 +0000

    migrate to vscode

--- headchef/settings.py
+++ headchef/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-y@w=ss2uw8)t4@d*u8)-f##&+d046en4y)*!p+-h&$ukkk5m2t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-kristinekik-djangorecip-7rss1atlxp9.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 # Application definition
 
@@ -105,7 +105,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net/",
+    "https://*.localhost",
     "https://*.herokuapp.com"
 ]
 
